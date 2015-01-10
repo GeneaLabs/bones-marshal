b3685aa36adc297c8491baf8fc0202c1bd5d8234
Mike Bronner <mike.bronner@genealabs.com>
2015-01-10 14:51:49 -0700

    Update BonesMarshalBaseModel.php

--- src/Genealabs/Bones/Marshal/BonesMarshalBaseModel.php
+++ src/Genealabs/Bones/Marshal/BonesMarshalBaseModel.php
@@ -4,10 +4,6 @@
 use GeneaLabs\Bones\Marshal\Events\EventGenerator;
 use Watson\Validating\ValidatingTrait;
 
-/**
- * Class BaseModel
- * @package GeneaLabs\Bones\Keeper\Models
- */
 class BonesMarshalBaseModel extends \BaseModel
 {
     use ValidatingTrait;
